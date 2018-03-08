68a96b3da71a97b9712664411af551eced88173f
Sean Coley <sean.coley@xero.com>
2018-03-08 16:39:58 +1300

    scoreKeeper app final commit

--- javascript-basics/Events/Event Listeners/scoreKeeper.js
+++ javascript-basics/Events/Event Listeners/scoreKeeper.js
@@ -1,67 +1,114 @@
-var winLimit;
-var limitReached = false;
-var player1 = 0;
-var player2 = 0;
+function app() {
 
-// function to update score variable
-function updateScore (id) {
-	if (id === "#player-1-increment") {
-		player1 ++;
-	} else if (id === "#player-2-increment") {
-		player2 ++;
-	};
-};
+	var winLimit = 5;
+	var limitReached = false;
+	var limitApplied = false;
+	var player1 = 0;
+	var player2 = 0;
 
-function updateVars (e) {
+	// function to update score variable
+	function updateScore(id) { console.log("run updateScore");
 
-	// update winLimit var
-	if (e.srcElement.id === "win-limit-input") {
-		winLimit = e.target.value;
-	};
+		console.log("updateScore player1: ", player1);
+		console.log("updateScore id: ", id);
 
-	if (e.srcElement.id ===)
-	updateScore(e.srcElement.id);
-
-	if (player1 >== winLimit || player2 >== winLimit) {
-
-	};
-};
-
-// set the win-limit span upon entering text value in the input box
-document.querySelector("#win-limit-input").addEventListener("change", e => { console.log("win-limit set")
-	winLimit = e.target.value;
-	document.querySelector("#win-limit").innerText = winLimit;
-	reset();
-	if (winLimit === 0) {
-		for (var i = 1; i <= 2; i++) {
-			winLimitReached("#player-" + i + "-score");
+		if (!limitReached) {
+			// increment player scores
+			if (id === "player-1-increment") {
+				player1 ++;
+			} else if (id === "player-2-increment") {
+				player2 ++;
+			};
 		};
+
 	};
-});
 
-// set the player 1 score upon clicking the player-1-increment button
-document.querySelector("#player-1-increment").addEventListener("click", e => {
-	var score = Number(document.querySelector("#player-1-score").innerText) + 1;
-	document.querySelector("#player-1-score").innerText = String(score);
-});
-
-// set the player 2 score upon clicking the player-2-increment button
-document.querySelector("#player-2-increment").addEventListener("click", e => {
-	var score = Number(document.querySelector("#player-2-score").innerText) + 1;
-	document.querySelector("#player-2-score").innerText = String(score);
-});
-
-// reset the scores upon clicking the reset button
-document.querySelector("#reset").addEventListener("click", e => {
-	reset();
-});
-
-
-// function to reset score spans to zero
-function reset() {
-	console.log("reset run");
-	var spans = document.querySelectorAll(".score-span");
-	for (var i = 0; i < spans.length; i++) {
-		spans[i].innerText = "0";
+	// functions to turn limit class on/off
+	function addLimitClass(cl) {
+		document.getElementsByClassName(cl)[0].classList.add("limit");
+		return cl;
 	};
+	function removeLimitClass(id) {
+		document.getElementById(id).classList.remove("limit");
+	};
+
+	// function to reset score spans to zero
+	function reset() {
+
+		console.log("reset run");
+
+		var spans = document.querySelectorAll(".score-span");
+
+		for (var i = 0; i < spans.length; i++) {
+			spans[i].innerText = "0";
+			removeLimitClass(spans[i].id)
+		};
+
+	};
+
+	// function to update DOM
+	function updateDOM() {
+
+		// update score counter
+		document.getElementById("player-1-score").innerText = String(player1);
+		document.getElementById("player-2-score").innerText = String(player2);
+
+		// update winlimit element
+		document.getElementById("win-limit").innerText = String(winLimit);
+
+		// class update
+		if (limitReached && !limitApplied) {
+			console.log("limitReached:", limitReached)
+			limitApplied = addLimitClass(limitReached);
+		};
+
+		if (targetId === "reset") {
+			reset();
+		}
+
+	};
+
+	// function to update all vars upon click of any element
+	function updateVars(e) {
+
+		targetId = e.srcElement.id
+
+		if (targetId === "reset") {
+			console.log("reset vars run");
+			var winLimit = 5;
+			var limitReached = false;
+			var limitApplied = false;
+			var player1 = 0;
+			var player2 = 0;
+			console.log("reset player1: ", player1);
+		};
+
+		// update winLimit var
+		if (targetId === "win-limit-input") {
+			if (e.target.value !== 0 && !isNaN(Number(e.target.value))) {
+				winLimit = Number(e.target.value);
+			} else {
+				alert("Please enter a number greater than zero!")
+			};
+		};
+
+		// run update score function if increment button clicked and limit not reached
+		if (targetId.search("/increment/")) {
+			console.log("targetId in updateScore if: ", targetId);
+			if (!limitReached) {
+				updateScore(targetId);
+			};
+		};
+
+		if (player1 >= winLimit || player2 >= winLimit) {
+			limitReached = targetId;
+		};
+
+		updateDOM();
+
+	};
+
+	// add event listeners
+	document.getElementById("clickables").addEventListener("click", e => {console.log("clickable clicked: ", e.target);updateVars(e);});
+	document.getElementById("win-limit-input").addEventListener("change", e => {updateVars(e)});
 };
