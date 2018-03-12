f7a5962d21f676a77a2b368d8ee96fe0aec5d6c7
Sean Coley <sean.coley@xero.com>
2018-03-13 11:34:34 +1300

    colourGame dynamic colours implemented

--- javascript-basics/colorGame/colourGame.js
+++ javascript-basics/colorGame/colourGame.js
@@ -1,17 +1,28 @@
-var colours = [
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)"
-]
+var colours = generateRandomColours(6);
 var squares = document.querySelectorAll(".square");
-var pickedColour = colours[3];
+var pickedColour = pickColour();
 var colourDisplay = document.getElementById("colour-display");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
 
-colourDisplay.textContent = pickedColour
+colourDisplay.textContent = pickedColour;
+
+resetButton.addEventListener("click", function() {
+	// generate all new colours
+	colours = generateRandomColours(6);
+	// pick new random colour from array
+	pickedColour = pickColour();
+	//console.log("colourIndex: ", colourIndex);
+	console.log("pickedColour: ", pickedColour);
+	// set colour display to picked colour
+	colourDisplay.textContent = pickedColour;
+	// change colours of squares
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colours[i];
+	};
+	h1.style.backgroundColor = "#232323";
+});
 
 for(var i = 0; i < squares.length; i++) {
 	// add initial colours to squares
@@ -19,7 +30,6 @@ for(var i = 0; i < squares.length; i++) {
 
 	// add click listeners to squares
 	squares[i].addEventListener("click", function() {
-		console.log("clicked a square")
 		// get colour of clicked square
 		var clickedColour = this.style.backgroundColor;
 		// compare clicked colour to picked colour (hehe)
@@ -29,9 +39,9 @@ for(var i = 0; i < squares.length; i++) {
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
-		}
+		};
 	});
-}
+};
 
 function changeColours(colour) {
 	// loop through squares and set them to pickedColour
@@ -39,5 +49,38 @@ function changeColours(colour) {
 		squares[i].style.backgroundColor = colour;
 	};
 	// set H1 background to be pickedColour
-	document.querySelector("h1").style.backgroundColor = colour;
+	h1.style.backgroundColor = colour;
+};
+
+function returnRandom(num) {
+	var random = Math.floor(Math.random() * num + 1);
+	return random;
+};
+
+function pickColour() {
+	var random = Math.floor(Math.random() * colours.length);
+	console.log("colours.length: ",colours.length);
+	return colours[random];
+};
+
+function generateRandomColours(num) {
+	// make an array
+	var arr = [];
+	for(var i = 0; i < num; i++) {
+		// get random colour and push into arr
+		arr.push(randomColour());
+	};
+	return arr
+};
+
+function randomColour() {
+	// pick a "red" from 0-255
+	var r = returnRandom(255);
+	// pick a "green" from 0-255
+	var g = returnRandom(255);
+	// pick a "blue" from 0-255
+	var b = returnRandom(255);
+	// concat string
+	var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+	return rgb;
 }
